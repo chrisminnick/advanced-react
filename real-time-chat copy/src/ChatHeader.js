b6f688c09e3fe8b93141df51cb0c519c0fe28aa5
Chris Minnick <chris@minnick.com>
2023-09-01 15:11:55 -0700

    removing copy of client

--- real-time-chat copy/src/ChatHeader.js
+++
@@ -1,9 +0,0 @@
-function ChatHeader() {
-  return (
-    <header className="Chat-header">
-      <h1>Realtime Chat</h1>
-    </header>
-  );
-}
-
-export default ChatHeader;
--- real-time-chat copy/src/socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client';
-
-export const socket = io('http://localhost:8081');
-
-export default socket;
