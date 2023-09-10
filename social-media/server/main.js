2247084ce497d351c8dac0dd54f36bf9d234100f
Chris Minnick <chris@minnick.com>
2023-09-10 06:39:51 -0700

    removing typescript

--- social-media/server/main.js
+++ social-media/server/main.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root'),
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
