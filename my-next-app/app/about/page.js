fbade3cb60569963c4176ec2a27f450625f6fa5f
Chris Minnick <chris@minnick.com>
2024-01-06 14:26:48 -0800

    added next demo

---
+++ my-next-app/app/about/page.js
@@ -0,0 +1,9 @@
+import Counter from './Counter';
+export default function Page() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <Counter />
+    </>
+  );
+}
---
+++ my-next-app/app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <Link href="/about">About Us</Link>
+        <h1>My Next.js App</h1>
+      </div>
+    </main>
+  );
+}
