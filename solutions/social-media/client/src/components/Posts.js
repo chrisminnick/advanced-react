f041a8f9d06561ec4e77f75124644418396e72eb
Chris Minnick <chris@minnick.com>
2024-01-09 08:09:33 -0800

    added social media client (hooks)

---
+++ solutions/social-media/client/src/components/Posts.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../provider/authProvider.js';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const { currentUser, setAuth } = useAuth();
+  const { token } = currentUser;
+  useEffect(
+    function getData() {
+      async function fetchPosts() {
+        try {
+          const response = await fetch('http://localhost:8081/api/posts', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + token,
+            },
+          });
+          const postData = await response.json();
+          setPosts(postData.posts);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      fetchPosts();
+    },
+    [token]
+  );
+
+  return (
+    <>
+      <h1>Latest Posts</h1>
+      {posts.map((post, index) => (
+        <p>{post.text}</p>
+      ))}
+      <Link to="/logout">Log out</Link>
+    </>
+  );
+}
+
+export default Posts;
